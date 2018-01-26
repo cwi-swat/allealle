c27845bc6862015bb21236d8408e1592a75ca7e0
Jouke Stoel <j.h.stoel@gmail.com>
2018-01-26 15:10:57 +0100

    Added a JUnit test runner for all the relation tests

---
+++ src/translation/tests/relationTests/RelationTestRunner.java
@@ -0,0 +1,11 @@
+package translation.tests.relationTests;
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestScheme;
+
+@RunWith(RascalJUnitTestRunner.class)
+@RascalJUnitTestPrefix("translation::tests::relationTests")
+@RascalJUnitTestScheme("cwd") 
+public class RelationTestRunner {}
